220c79d80b58cd45821e350e994204decafa6ced
DevonGifford <DevonGifford@outlook.com>
2023-03-25 22:30:36 +0100

    minor updates - cleaning up in prep for passwords

--- server.js
+++ server.js
@@ -96,10 +96,10 @@ app.listen(3000, ()=> {
 /* 
 planning API - what will our design look like
 
-route/home rout 	-->  GET request 	(data)			-->  response 	:  this is working ✔
-✔sign-in route  		-->  POST request 	(JSON) 			-->  response 	:  success  or  if failure = register route
+✔route/home rout 	-->  GET request 	(data)			-->  response 	:  this is working ✔
+✔sign-in route  	-->  POST request 	(JSON) 			-->  response 	:  success  or  if failure = register route
 ✔register route		-->  POST request 	(database)		-->  response 	:  user object
 ✔profile/(userID)	-->  GET request 	(user)			-->  response 	:  user information
-Ranking				-->  PUT request 	(user data)		-->  update 	:  user object (count)
+✔Ranking				-->  PUT request 	(user data)		-->  update 	:  user object (count)
 
 */
