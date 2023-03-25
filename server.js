28e6626916b772648c1335945ebb24caf409da12
DevonGifford <DevonGifford@outlook.com>
2023-03-25 19:02:30 +0100

    creating basic routes: profile(Id), updating image count

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.get('/', (req, res) =>{
 	res.send(test_database.users);
 })
 
+
+app.post('/signin', (req, res) => {
+if (req.body.email === test_database.users[0].email && 
+req.body.password === test_database.users[0].password) {
+	res.json('sign in success');
+}else {
+	res.status(400).json('error logging in');
+}
+})
+	
+	
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
 	test_database.users.push({
@@ -44,30 +55,51 @@ app.post('/register', (req, res) => {
 	res.json(test_database.users[test_database.users.length-1]);
 })
 
-app.post('/signin', (req, res) => {
-	if (req.body.email === test_database.users[0].email && 
-		req.body.password === test_database.users[0].password) {
-			res.json('sign in success');
-		}else {
-			res.status(400).json('error logging in');
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let foundUser = false; 
+	test_database.users.forEach(user => {
+		if (user.id === id) {
+			foundUser = true;
+			return res.json(user);	
 		}
+	})
+	if (!foundUser) {
+		res.status(400).json('no user found');
+	}
+})
+
+app.put('/image', (req, res)=> {
+	const { id } = req.body;
+	let foundUser = false; 
+	test_database.users.forEach(user => {
+		if (user.id === id) {
+			foundUser = true;
+			user.entries++
+			return res.json(user.entries);	
+		}
+	})
+	if (!foundUser) {
+		res.status(400).json('no user found - thus error');
+	}
 })
 
 app.listen(3000, ()=> {
 	console.log('dev test- app is running on port 3000');
 })
 
-
-
+	
+	
 
 
 /* 
 planning API - what will our design look like
 
-route/home rout 	-->  GET request 	(data)			-->  response 	:  this is working 
-sign-in route  		-->  POST request 	(JSON) 			-->  response 	:  success  or  if failure = register route
-register route		-->  POST request 	(database)		-->  response 	:  user object
-profile/(userID)	-->  GET request 	(user)			-->  response 	:  user information
+route/home rout 	-->  GET request 	(data)			-->  response 	:  this is working ✔
+✔sign-in route  		-->  POST request 	(JSON) 			-->  response 	:  success  or  if failure = register route
+✔register route		-->  POST request 	(database)		-->  response 	:  user object
+✔profile/(userID)	-->  GET request 	(user)			-->  response 	:  user information
 Ranking				-->  PUT request 	(user data)		-->  update 	:  user object (count)
 
 */
